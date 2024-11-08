a3097178a8b5e4319a3b918e92b5476b8fe9eaeb
Olaseni Bello <Olasenibello38@gmail.com>
2024-11-08 17:25:22 -0500

    Fixing up the Db Settings

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const app = express();
 const { Sequelize } = require('sequelize');
@@ -6,16 +5,34 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const waitlistRoutes = require('./routes/waitlistRoutes');
 const userRoutes = require('./routes/userRoutes');
-const notificationRoutes = require('./routes/notificationRoutes'); 
+const notificationRoutes = require('./routes/notificationRoutes');
 
 const PORT = process.env.PORT || 5000;
 
-const sequelize = new Sequelize('waitlist_app', 'root', 'root', {
+let sequelize;
+
+if (process.env.NODE_ENV === 'production') {
+  // Use JawsDB MySQL database in production
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'mysql',
+    protocol: 'mysql',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // If using self-signed certificates
+      }
+    }
+  });
+} else {
+  // Use local MySQL database in development
+  sequelize = new Sequelize('waitlist_app', 'root', 'root', {
     host: '127.0.0.1',
     dialect: 'mysql',
-    "port": 8889
+    port: 8889
   });
-  sequelize.authenticate()
+}
+
+sequelize.authenticate()
   .then(() => console.log('Database connected...'))
   .catch(err => console.error('Database connection error:', err));
 
@@ -24,10 +41,9 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
-app.use ('/api/waitlists',waitlistRoutes);
+app.use('/api/waitlists', waitlistRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/notifications', notificationRoutes);
-// app.get('/', "Welcome to My App");
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
